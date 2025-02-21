b4a7e2e4e4d4edb9ff8e73675954277cfc2a31a8
Aditya Rawat <aadityarawatt@gmail.com>
2025-02-21 19:34:03 +0530

    mark as completed functionality added

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect,get_object_or_404
+
 from .models import Tasks
+from django.http import HttpResponse
 # Create your views here.
 def addTask(request):
     task=request.POST['task']
     Tasks.objects.create(task=task)
+    return redirect("home")
+
+def markAsCompelted(request,pk):
+    task=get_object_or_404(Tasks, pk=pk)
+    task.is_completed=True
+    task.save()
     return redirect("home")
