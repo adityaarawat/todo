bb33786cf7dd843fe21662d4d9213f75dd24a10e
Aditya Rawat <aadityarawatt@gmail.com>
2025-02-21 16:31:56 +0530

    fetched data from models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Tasks(models.Model):
+    task=models.CharField(max_length=250)
+    is_completed=models.BooleanField(default=False)
+    created_at=models.BooleanField(default=True)
+    updated_at=models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.task
--- todoMain/views.py
+++ todoMain/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
-
+from todo.models import Tasks
 def home(request):
-    return render(request,'index.html')
+    task=Tasks.objects.filter(is_completed=False)
+    completed_Task=Tasks.objects.filter(is_completed=True)
+    context={
+        'tasks':task,
+        'completeTask':completed_Task
+    }
+    return render(request,'index.html',context)
